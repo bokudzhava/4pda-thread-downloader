716a965022dd977653c36b07953665f19b818444
bokudzhava <95191283+bokudzhava@users.noreply.github.com>
2025-12-06 02:31:08 +0300

    Update and rename 4pda-thread-downloader-1.2.meta.js to 4pda-thread-downloader-1.3.meta.js

--- 4pda-thread-downloader-1.3.meta.js
+++ 4pda-thread-downloader-1.3.meta.js
@@ -2,14 +2,14 @@
 // @name         4pda Thread Downloader
 // @namespace    4PDA
 // @homepage     https://github.com/bokudzhava/4pda-thread-downloader
-// @version      1.2
+// @version      1.3
 // @description  Скачивает ветку 4PDA в формате TXT
 // @author       bokudzhava
 // @match        https://4pda.to/forum/*
 // @match        https://4pda.ru/forum/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=4pda.to
-// @downloadURL  https://github.com/bokudzhava/raw/main/4pda-thread-downloader-1.2.user.js
-// @updateURL    https://github.com/bokudzhava/raw/main/4pda-thread-downloader-1.2.meta.js
+// @downloadURL  https://github.com/bokudzhava/raw/main/4pda-thread-downloader-1.3.user.js
+// @updateURL    https://github.com/bokudzhava/raw/main/4pda-thread-downloader-1.3.meta.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js
 // @grant        none
 // ==/UserScript==
